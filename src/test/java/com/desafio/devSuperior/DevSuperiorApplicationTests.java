2b7304ca2411d1d15cde4a7fcc1c6c62519be8ff
Samuel Oliveira <samuel.oleiv@gmail.com>
2025-12-10 16:51:00 -0300

    Iniciado a estrutura do Projeto

---
+++ src/test/java/com/desafio/devSuperior/DevSuperiorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.desafio.devSuperior;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DevSuperiorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
