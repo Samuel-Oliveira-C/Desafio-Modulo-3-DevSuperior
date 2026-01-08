39ff1f13c106d3c1f3a0485d36c36b6363051865
Samuel Oliveira <samuel.oleiv@gmail.com>
2026-01-08 11:42:12 -0300

    Foi feito a busca paginada tanto no service quanto no controller

--- src/main/java/com/desafio/devSuperior/controllers/ClientController.java
+++ src/main/java/com/desafio/devSuperior/controllers/ClientController.java
@@ -5,6 +5,9 @@ import org.springframework.web.bind.annotation.RestController;
 
 import com.desafio.devSuperior.model.dto.ClientDTO;
 import com.desafio.devSuperior.services.ClientService;
+
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
@@ -13,7 +16,7 @@ import org.springframework.web.bind.annotation.PathVariable;
 @RestController
 @RequestMapping(value = "/clients")
 public class ClientController {
-    public final ClientService service;
+    private final ClientService service;
 
     public ClientController(ClientService service) {
         this.service = service;
@@ -22,6 +25,11 @@ public class ClientController {
     @GetMapping("/{id}")
     public ClientDTO findById(@PathVariable Long id) {
         return service.findById(id);
+    }
+    
+    @GetMapping
+    public Page<ClientDTO> findAllBySorting(Pageable list){
+        return service.findAllBySorting(list);
     }
     
 }
--- src/main/java/com/desafio/devSuperior/services/ClientService.java
+++ src/main/java/com/desafio/devSuperior/services/ClientService.java
@@ -1,15 +1,20 @@
 package com.desafio.devSuperior.services;
 
+
+
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 
 import com.desafio.devSuperior.model.dto.ClientDTO;
+import com.desafio.devSuperior.model.entity.Client;
 import com.desafio.devSuperior.repository.ClientRepository;
 
 import org.springframework.transaction.annotation.Transactional;
 
 @Service
 public class ClientService {
-    public final ClientRepository repository;
+    private final ClientRepository repository;
 
     public ClientService(ClientRepository repository) {
         this.repository = repository;
@@ -17,5 +22,12 @@ public class ClientService {
 
     @Transactional(readOnly = true)
     public ClientDTO findById(Long id) {
-        return new ClientDTO(repository.findById(id).get());}
+        return new ClientDTO(repository.findById(id).get());
+    }
+
+    @Transactional(readOnly = true)
+    public Page<ClientDTO> findAllBySorting(Pageable listPageable){
+        Page<Client> result = repository.findAll(listPageable);
+        return result.map(x -> new ClientDTO(x));
+    }
 }
